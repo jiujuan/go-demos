270b6d89b0d55eda9ba1d70380a2fc5ba3eb8066
九卷 <jiujuanfeng@163.com>
2024-01-02 23:51:30 +0800

    fmt code

--- algorithms-go/linked-list/singlelinkedlist.go
+++ algorithms-go/linked-list/singlelinkedlist.go
@@ -1,13 +1,29 @@
 package main
 
 type ListNode struct {
-	Val interface{}
+	Val  interface{}
 	Next *ListNode
+}
+
+type LinkedList struct {
+	Head *ListNode // 指向第一个节点
+	Len  int
 }
 
 func NewListNode(val interface{}) *ListNode {
 	return &ListNode{
-		Val: val,
+		Val:  val,
 		Next: nil,
 	}
+}
+
+func NewLinkedList() *LinkedList {
+	return &LinkedList{
+		Head: NewListNode(0),
+		Len:  0,
+	}
+}
+
+func (l *LinkedList) Insert(p *ListNode, val interface{}) bool {
+
 }
